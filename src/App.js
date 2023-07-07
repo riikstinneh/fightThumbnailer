3fac3fa8f5fc6959dbc8881861739a06258ad4e6
venfaaniik <kiu.ude@gmail.com>
2023-07-07 22:01:54 +0300

    footer and css changes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,55 @@
-import ImageEditor from "./components/ImageEditor";
-import { useState, useEffect } from "react";
+import ImageEditor from "./components/ImageEditor"
+import { useState } from "react"
+import Footer from "./components/Footer"
 
 function App() {
-	const [image, setImage] = useState(null);
-	const [selectedBorder, setSelectedBorder] = useState(() => {
-		// Load saved border images from local storage
-		const savedBorders = localStorage.getItem("customBorders");
-		const initialBorder = JSON.parse(savedBorders);
-		return initialBorder || null;
-	});
+    const [image, setImage] = useState(null)
+    const [selectedBorder, setSelectedBorder] = useState(() => {
+        // Load saved border images from local storage
+        const savedBorders = localStorage.getItem("customBorders")
+        const initialBorder = JSON.parse(savedBorders)
+        return initialBorder || null
+    })
 
-	const onImageChange = (e) => {
-		setImage(URL.createObjectURL(e.target.files[0]));
-	};
+    const onImageChange = (e) => {
+        setImage(URL.createObjectURL(e.target.files[0]))
+    }
 
-	const handleBorderUpload = (e) => {
-		const selectedFile = e.target.files[0];
-		const reader = new FileReader();
+    const handleBorderUpload = (e) => {
+        const selectedFile = e.target.files[0]
+        const reader = new FileReader()
 
-		reader.onload = (function (f) {
-			return function (e) {
-				const newBorder = { imageUrl: e.target.result };
-				setSelectedBorder(newBorder);
+        reader.onload = (function (f) {
+            return function (e) {
+                const newBorder = { imageUrl: e.target.result }
+                setSelectedBorder(newBorder)
 
-				localStorage.clear();
-				localStorage.setItem("customBorders", JSON.stringify(newBorder));
-			};
-		})(selectedFile);
+                localStorage.clear()
+                localStorage.setItem("customBorders", JSON.stringify(newBorder))
+            }
+        })(selectedFile)
 
-		reader.readAsDataURL(selectedFile);
-	};
+        reader.readAsDataURL(selectedFile)
+    }
 
-	return (
-		//max-w-7xl
-		<div className="grid h-screen place-items-center">
-			<div className="card mx-auto flex bg-stone-800">
-				<div className="flex flex-row">
-					<div className="">
-						<ImageEditor
-							image={image}
-							selectedBorder={selectedBorder}
-							onImageChange={onImageChange}
-							handleBorderUpload={handleBorderUpload}
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        //max-w-7xl
+        <div className="grid h-screen place-items-center">
+            <div className="card mx-auto flex bg-stone-800">
+                <div className="flex flex-row">
+                    <ImageEditor
+                        image={image}
+                        selectedBorder={selectedBorder}
+                        onImageChange={onImageChange}
+                        handleBorderUpload={handleBorderUpload}
+                    />
+                </div>
+            </div>
+            <div className="fixed inset-x-0 bottom-0">
+                <Footer />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
